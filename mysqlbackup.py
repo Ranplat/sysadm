2fc6fb2e7f8708fc97e1c9c97ec51faa4be33e27
Ranplat <35362527+Ranplat@users.noreply.github.com>
2019-04-17 18:21:29 +0800

    Update mysqlbackup.py

--- mysqlbackup.py
+++ mysqlbackup.py
@@ -109,3 +109,13 @@ def main():
 if __name__ == '__main__':
     main()
 #bakimeeting.bakData('backup_file')
+
+'''
+配置文件conf.py如下：
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+conn_dict = dict(host='127.0.0.1',user='root',passwd='12345',db='08day05',port = 33061)
+contact_user = dict(address='smtp.163.com',sender='xxxxxx@163.com',password='xxxxxx')
+note_user = ('xxxxxx@126.com',)
+backup_path = "D:/KuGou/"
+'''
